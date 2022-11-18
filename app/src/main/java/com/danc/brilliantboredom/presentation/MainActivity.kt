7be8ad14c276057d9bf24fd9c20e35e566514436
Danc-0 <lunjaludaniel@gmail.com>
2022-11-18 11:39:19 +0300

    code update

--- app/src/main/java/com/danc/brilliantboredom/presentation/MainActivity.kt
+++ app/src/main/java/com/danc/brilliantboredom/presentation/MainActivity.kt
@@ -1,31 +1,28 @@
 package com.danc.brilliantboredom.presentation
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.danc.brilliantboredom.presentation.components.BoredActivityItem
-import com.danc.brilliantboredom.presentation.components.SnackBars
 import com.danc.brilliantboredom.presentation.theme.BrilliantBoredomTheme
 import com.danc.brilliantboredom.presentation.viewmodel.GetBoredActivitiesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
-import kotlin.math.log
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
